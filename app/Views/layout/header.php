66d402ef9cfd9dfd8390353b607bb47901d20a3c
ravino rahman <raviorahman@gmail.com>
2021-04-28 22:07:43 +0700

    merubah tulisan home

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -20,7 +20,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mr-auto mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link px-4" aria-current="page" href="/simulasi">Home</a>
+                        <a class="nav-link px-4" aria-current="page" href="/simulasi">HomeCoy</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active" href="/simulasi">Simulasi</a>
