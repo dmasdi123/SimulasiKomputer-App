fb240ca7ca324172d01d95492b676968fa9cf098
ravino rahman <raviorahman@gmail.com>
2021-04-29 19:29:29 +0700

    perubahan dorpdown vga

--- app/Views/pages/simulasi.php
+++ app/Views/pages/simulasi.php
@@ -62,8 +62,8 @@
         </div>
         <div class="row mb-3">
             <div class="col-md-7 sm-12 mt-2">
-                <select class="form-select" id="proc" aria-label="Default select example" name="proc">
-                    <option selected>Open this select menu</option>
+                <select class="form-select" id="vga" aria-label="Default select example" name="vga">
+                    <option selected>VGA</option>
                     <option value="1">One</option>
                     <option value="2">Two</option>
                     <option value="3">Three</option>
